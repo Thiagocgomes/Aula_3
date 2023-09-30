575a73e85babda0cb00065553ff371f69f88b1b6
Thiago de Carvalho <145858546+Thiagocgomes@users.noreply.github.com>
2023-09-30 16:29:22 -0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,121 @@
+# numero1 = int(input('Digite um número: '))
+# numero2 = numero1 * numero1
+# print(numero2)
+
+# nome = "Thiago"
+# sobrenome = "Carvalho"
+# print (nome, sobrenome)
+
+# n1 = input('Digite um número: ')
+# n2 = input('Digite o segundo número: ')
+# print ("numeros: ", n1,n2)
+
+# palavra = "Python"
+# numero1 = "10"
+# print (palavra,numero1)
+
+# palavra = 'Boa Tarde'
+# palavra2 = input('Digite uma frase: ')
+# print (palavra,palavra2)
+
+# nome = input('Digite aqui o seu nome ->')
+# idade = input('Digite aqui sua idade ->')
+# endereco = input('Dite aqui seu endereço ->')
+# cargo = input('Digite aqui a sua profissão ->')
+# print('Meu nome é', nome, 'minha idade é', idade, 'meu endereço é', endereco, 'meu cargo é', cargo)
+# print(f'Meu nome é {nome}, minha idade é {idade}, meu endereço é {endereco}, meu cargo é {cargo}')
+
+# hora = int(input('Digite a hora: '))
+# minuto = int(input('Digite o minuto: '))
+# segundo = int(input('Digite o segundo: '))
+# print (f'Horario: {hora}:{minuto}:{segundo}')
+
+# codigo = input('Digite o código de área: ')
+# ddd = input('Digite o seu DDD: ')
+# telefone = input('Digite o seu telefone: ')
+# print(f'Meu numéro de telefone é ({codigo}){ddd}{telefone}')
+
+# ovo = 'Ovo'
+# farinha = 'farinha'
+# fermento = 'fermento'
+# cenoura = 'cenoura'
+# print (f'Lista de ingredientes para fazer um bolo: {ovo},{farinha},{fermento},{cenoura}')
+
+# adjetivo_1 = input('Digite um adjetivo ->')
+# adjetivo_2 = input('Digite o segundo adjetivo ->')
+# adjetivo_3 = input('Digite o terceiro adjetivo ->')
+# print (f'Você é {adjetivo_1},{adjetivo_2},{adjetivo_3}')
+
+
+# numero = float(input('Digite um número: '))
+
+# if numero == 10:
+#    print(f'O numero {numero}')
+# else:
+#    print(f'O número é {numero}')
+
+
+# senha_digitada = input('Digite uma senha: ')
+# nome = input('Digite seu nome: ')
+# senha = '123456'
+
+# if senha == senha_digitada and nome == 'Thiago':
+#      print('Acesse, você digitou corretamente!')
+# else:
+#      print('Não pode acessar, porque você digitou {senha} ou {nome} incorretamente!')
+
+
+# idade = 45
+# idade2 = 18
+
+# if idade > idade2:
+#   print('A idade 1 é maior')
+# else:
+#   print('A idade 2 é menor')
+
+# idade = int(input('Digite a sua idade: '))
+
+# if idade >= 18:
+#   print('Você é maior de idade, pode entrar na festa!')
+# else:
+#   print('Volta pra casa, você é menor de idade')
+
+# nota = int(input('Digite a sua primeira nota: '))
+# nota1 = int(input('Digite a sua segunda nota: '))
+# nota3 = int(input('Digite a sua terceira nota: '))
+# media = (nota + nota1 + nota3)/3
+
+# if media >= 6:
+#   print('Parabéns, você passou')
+#   print(f' Sua nota é {media}')
+# else:
+#   print('Você repetiu de ano')
+
+
+  
+  
+  
+  
+  
+
+
+  
+
+
+
+  
+
+
+ 
+
+
+
+
+
+
+
+
+
+
+
+
